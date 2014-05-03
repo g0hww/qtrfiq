74a80031e8595b42f89be5031a7cec88594f9b94
Darren Long <darren.long@mac.com>
2014-05-03 09:49:38 +0100

    Initial commit of stuff.

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 qtrfiq
 ======
 
-GRC flowgraph and Python script for QT GUI with rigctrl, fosphor and IQ audio input
+A GRC flowgraph and its python script for a QT GUI with a gr-fosphor display 
+that uses hamlib's rigctrl and an IQ audio input.
+
+qtrfiq, pronounced "q terrific", consists of a simple GRC flowgraph and its 
+generated python script.  There's nothing at all complicated in the flowgraph,
+other than a nifty bodge with a pair of function probe blocks that enables
+qtrfiq to poll the radio's frequency using  hamlib's rigctl utility.
+To use qtrfiq, you need to have gnuradio and gr-fosphor installed.
+
+gnuradio: http://gnuradio.org/redmine/projects/gnuradio/wiki
+gr-fosphor: http://sdr.osmocom.org/trac/wiki/fosphor
+
+There is, however an issue with the generated python code, but it can be easily 
+fixed with some editing and will hopefully be fixed in GRC. GRC generates 
+invalid code, which then fails to run, for a FunctionProbe block when the
+BlockID is empty.  The issue is described in this bug: 
+
+http://gnuradio.org/redmine/issues/668
+
+You can generate the Python script and edit it to remove the superfluous dot 
+symbols from the function definitions of _SetSampleRateProbe_probe() and 
+_SetRigFreqProbe_probe() that can be found towards the end of the qtrfiq class 
+constructor.  I will have probably remembered to do have done this myself when I
+have committed the generated python.
+
+The invocation of rigctl is performed in the SetRigFreqProbe FunctionProbe 
+block, and it's arguments can be modifed by changing the tuple passed into 
+the invocation of subprocess.check() in the block's "Function Args" property.
+By default, the arguments use a rig model of 2, corresponding to a rigctld 
+server on the default port.
+
+As there is no callback available from the QT fosphor sink, there is no 
+click-to-tune capability, or any other means yet to control the radio's
+frequency from qtrfiq.
---
+++ qtrfiq.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+##################################################
+# Gnuradio Python Flow Graph
+# Title: qtrfiq.py
+# Author: Darren Long, G0HWW
+# Description: QT GUI with rigctrl, fosphor and I?Q audio input
+# Generated: Sat May  3 09:48:12 2014
+##################################################
+
+from PyQt4 import Qt
+from gnuradio import audio
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import fosphor
+from gnuradio import gr
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from optparse import OptionParser
+import sip
+import subprocess
+import sys
+import threading
+import time
+
+class qtrfiq(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "qtrfiq.py")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("qtrfiq.py")
+        try:
+             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+             pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "qtrfiq")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
+
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.rig_freq = rig_freq = float(subprocess.check_output(['/usr/bin/rigctl', '-m', '2', 'f']).strip())
+        self.samp_rate = samp_rate = 96000
+        self.freq = freq = rig_freq
+        self.SetSampleRateProbe = SetSampleRateProbe = 1
+        self.SetRigFreqProbe = SetRigFreqProbe = 0
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self._freq_tool_bar = Qt.QToolBar(self)
+        self._freq_tool_bar.addWidget(Qt.QLabel("Frequency"+": "))
+        self._freq_line_edit = Qt.QLineEdit(str(self.freq))
+        self._freq_tool_bar.addWidget(self._freq_line_edit)
+        self._freq_line_edit.returnPressed.connect(
+        	lambda: self.set_freq(eng_notation.str_to_num(self._freq_line_edit.text().toAscii())))
+        self.top_grid_layout.addWidget(self._freq_tool_bar, 0,0,1,1)
+        self.fosphor_qt_sink_c_0 = fosphor.qt_sink_c()
+        self.fosphor_qt_sink_c_0.set_fft_window(window.WIN_BLACKMAN_hARRIS)
+        self.fosphor_qt_sink_c_0.set_frequency_range(rig_freq, samp_rate)
+        self._fosphor_qt_sink_c_0_win = sip.wrapinstance(self.fosphor_qt_sink_c_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._fosphor_qt_sink_c_0_win)
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.audio_source_0 = audio.source(samp_rate, "", True)
+        def _SetSampleRateProbe_probe():
+        	while True:
+        		val = self.set_samp_rate(self.samp_rate)
+        		try: self.set_SetSampleRateProbe(val)
+        		except AttributeError, e: pass
+        		time.sleep(1.0/(20))
+        _SetSampleRateProbe_thread = threading.Thread(target=_SetSampleRateProbe_probe)
+        _SetSampleRateProbe_thread.daemon = True
+        _SetSampleRateProbe_thread.start()
+        def _SetRigFreqProbe_probe():
+        	while True:
+        		val = self.set_rig_freq(float(subprocess.check_output(['/usr/bin/rigctl','-m','2','f']).strip()))
+        		try: self.set_SetRigFreqProbe(val)
+        		except AttributeError, e: pass
+        		time.sleep(1.0/(5))
+        _SetRigFreqProbe_thread = threading.Thread(target=_SetRigFreqProbe_probe)
+        _SetRigFreqProbe_thread.daemon = True
+        _SetRigFreqProbe_thread.start()
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.audio_source_0, 1), (self.blocks_float_to_complex_0, 0))
+        self.connect((self.audio_source_0, 0), (self.blocks_float_to_complex_0, 1))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.fosphor_qt_sink_c_0, 0))
+
+
+# QT sink close method reimplementation
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "qtrfiq")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_rig_freq(self):
+        return self.rig_freq
+
+    def set_rig_freq(self, rig_freq):
+        self.rig_freq = rig_freq
+        self.fosphor_qt_sink_c_0.set_frequency_range(self.rig_freq, self.samp_rate)
+        self.set_freq(self.rig_freq)
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.fosphor_qt_sink_c_0.set_frequency_range(self.rig_freq, self.samp_rate)
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        Qt.QMetaObject.invokeMethod(self._freq_line_edit, "setText", Qt.Q_ARG("QString", eng_notation.num_to_str(self.freq)))
+
+    def get_SetSampleRateProbe(self):
+        return self.SetSampleRateProbe
+
+    def set_SetSampleRateProbe(self, SetSampleRateProbe):
+        self.SetSampleRateProbe = SetSampleRateProbe
+
+    def get_SetRigFreqProbe(self):
+        return self.SetRigFreqProbe
+
+    def set_SetRigFreqProbe(self, SetRigFreqProbe):
+        self.SetRigFreqProbe = SetRigFreqProbe
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    (options, args) = parser.parse_args()
+    Qt.QApplication.setGraphicsSystem(gr.prefs().get_string('qtgui','style','raster'))
+    qapp = Qt.QApplication(sys.argv)
+    tb = qtrfiq()
+    tb.start()
+    tb.show()
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.exec_()
+    tb = None #to clean up Qt widgets
+
